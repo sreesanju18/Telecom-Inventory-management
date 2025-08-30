0de78019c95a007f46a4c0ec1f1e4580a533ec4b
sreesanju18 <bsannnju@gmail.com>
2025-08-30 17:43:48 +0530

    inital

---
+++ app/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" h-screen w-screen flex items-center justify-center ">
+      <
+    </div>
+  );
+}
+
+export default Home;
